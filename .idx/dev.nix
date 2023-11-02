efe7a7fc4f40465591fd439acc059b03e6268fc9
asrospierski <asrospierski@gmail.com>
2023-11-02 04:08:45 +0000

    update clear & escape

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -16,14 +16,16 @@
   # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
   idx.extensions = [
     "vscodevim.vim"
+    "astro-build.astro-vscode"
   ];
 
   # Enable previews and customize configuration
   idx.previews = {
-    enable = true;
+    enable = false;
     previews = [
       {
         command = [
+          "npx"
           "astro"
           "dev"
         ];
